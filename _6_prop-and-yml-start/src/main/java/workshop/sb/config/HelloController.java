2ff9e7383d88f91df2c400532cdbc42a63bb05db
emilka <emilia.pietras94@gmail.com>
2018-12-12 14:54:55 +0100

    new changes

--- _6_prop-and-yml-start/src/main/java/workshop/sb/config/HelloController.java
+++ _6_prop-and-yml-start/src/main/java/workshop/sb/config/HelloController.java
@@ -1,16 +1,34 @@
 package workshop.sb.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    // TODO 4 zdefiniuj pola hello, msg, foo, wstrzyknij do nich wartości używając @Value
 
-    // TODO 5 utwórz metodę obsługującą GET, "/hello"
+    @Value("Hello World from properties!")
+    private String hello;
 
-    // TODO 7 utwórz metodę obsługującą GET, "/msg"
+    @Value("MSG from properties!")
+    private String msg;
 
-    // TODO 9 utwórz metodę obsługującą GET, "/foo"
+    @Value("foo")
+    private String foo;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return hello;
+    }
+
+    @GetMapping("/msg")
+    public String msg() {
+        return msg;
+    }
+
+    @GetMapping("/foo")
+    public String foo() {
+        return foo;
+    }
 }
-
 
